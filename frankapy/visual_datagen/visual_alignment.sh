94bc9100c83fe93fb019117696420895510f271c
bingwen <pancake.wbw@gmail.com>
2025-09-19 21:50:13 +0800

    add .sh files

---
+++ frankapy/visual_datagen/visual_alignment.sh
@@ -0,0 +1,11 @@
+export DIR_NAME="records/franka-track"
+export PREHEAT_TIME=5
+export RECORD_FRAMES=10
+export FPS=15
+export PROJECT_ROOT=$(pwd)
+
+python3 visual_datagen/cam_capture.py \
+--dir_name $DIR_NAME \
+--preheat_time $PREHEAT_TIME \
+--fps $FPS \
+--record_frames $RECORD_FRAMES 
